b63b87c9f074b19ffa73ed7eb3c2718efab5f5be
Chonfa <christianmmurga@gmail.com>
2019-09-02 15:58:05 -0300

    Update nuevo tipo de suelo

--- lunarRover.cpp
+++ lunarRover.cpp
@@ -35,14 +35,18 @@ class brazo
 		rpm = 0;
 	}
  	void extraer_muestra(double porosidadSuelo, double durezaSuelo){
-		if (porosidadSuelo<0.5 && durezaSuelo>0.5){
+		if (porosidadSuelo<0.35 && durezaSuelo>0.65){
 			this->girar_horario(150,10);
 			this->accionar_cierre();
 			this->girar_antihorario(150,10);
-		}else if (porosidadSuelo>0.5 && durezaSuelo<0.5){
+		}else if (porosidadSuelo>0.65 && durezaSuelo<0.35){
 			this->girar_antihorario(100,5);
 			this->accionar_cierre();
 			this->girar_horario(100,5);
+		}else if (porosidadSuelo>0.35 && porosidadSuelo<0.65 && durezaSuelo<0.65 && durezaSuelo<0.35){
+			this->girar_horario(150,5);
+			this->accionar_cierre();
+			this->girar_antihorario(100,10);
 		}else{
 			cerr<<"ERROR: tipo de suelo desconocido"<<endl;
 		}
