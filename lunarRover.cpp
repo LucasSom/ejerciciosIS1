e5e79a3d0fb9e1d96c609227a88c5accffc24193
Orfeo <lsomacal@gmail.com>
2019-09-02 13:28:54 -0300

    Ejercicio 0 - A

--- lunarRover.cpp
+++ lunarRover.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
 #include <stdio.h>
+#include <unistd.h>
 
 using namespace std;
 
 /*TODO:
-	-libreria de tiempo
-	-chequear
 	-tercera variedad de piso
-	- mensajes por consola
 */
 
 
 class brazo
  {
  public:
- 	brazo();
- 	~brazo();
- 	void girar_horario(int velocidad, int t);
- 	void girar_antihorario(int velocidad, int t);
- 	void accionar_cierre();
- 	void extraer_muestra(double porosidadSuelo, double durezaSuelo);
+ 	brazo(){
+		rpm = 0;
+		tiempo = 0;
+	}
+ 	~brazo(){
+ 		free(this);
+ 	}
+ 	void girar_horario(int velocidad, int t){
+		giroHorario = true;
+		rpm = velocidad;
+		sleep(t);
+		rpm = 0;
+	}
+ 	void girar_antihorario(int velocidad, int t){
+		giroHorario = false;
+		rpm = velocidad;
+		sleep(t);
+		rpm = 0;
+	}
+ 	void accionar_cierre(){
+		rpm = 0;
+	}
+ 	void extraer_muestra(double porosidadSuelo, double durezaSuelo){
+		if (porosidadSuelo<0.5 && durezaSuelo>0.5){
+			this->girar_horario(150,10);
+			this->accionar_cierre();
+			this->girar_antihorario(150,10);
+		}else if (porosidadSuelo>0.5 && durezaSuelo<0.5){
+			this->girar_antihorario(100,5);
+			this->accionar_cierre();
+			this->girar_horario(100,5);
+		}else{
+			cerr<<"ERROR: tipo de suelo desconocido"<<endl;
+		}
+	}
 
  private:
  	int rpm;
@@ -27,50 +54,22 @@ class brazo
 	int tiempo;
  };
 
-brazo::brazo(){
-	rpm = 0;
-	tiempo = 0;
-}
 
-void girar_horario(int velocidad, int t){
-	giroHorario = true;
-	rpm = velocidad;
-	sleep(t);
-	rpm = 0;
-}
-
-void girar_antihorario(int velocidad, int t){
-	giroHorario = false;
-	rpm = velocidad;
-	sleep(t);
-	rpm = 0;
-}
-
-void accionar_cierre(){
-	rpm = 0;
-}
-
-void extraer_muestra(double porosidadSuelo, double durezaSuelo){
-	if (porosidadSuelo<0.5 && durezaSuelo>0.5){
-		scioli.girar_horario(150,10);
-		scioli.accionar_cierre();
-		scioli.girar_antihorario(150,10);
-	}else if (porosidadSuelo>0.5 && durezaSuelo<0.5){
-		scioli.girar_antihorario(100,5);
-		scioli.accionar_cierre();
-		scioli.girar_horario(100,5);
-	}else{
-		cerr<<"ERROR: tipo de suelo desconocido"<<endl;
-	}
-}
 
 int main(int argc, char const *argv[]){
-	double durezaSuelo;
 	double porosidadSuelo;
+	double durezaSuelo;
 
 	brazo scioli;
 
-	scioli.extraer_muestra(porosidadSuelo, durezaSuelo);
+	while(1){
+		cout<<endl<<"Ingresar porosidad del suelo: ";
+		cin>>porosidadSuelo;
+		cout<<endl<<"Ingresar dureza del suelo: ";
+		cin>>durezaSuelo;
+		scioli.extraer_muestra(porosidadSuelo, durezaSuelo);
+	}
 	
+
 	return 0;
 }
